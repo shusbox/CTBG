ba1be34b2b399ad3a3b8ae953b24275ea9ab8702
shusbox <shusbox@naver.com>
2025-12-08 01:36:42 +0900

    feat :: register 페이지 추가

---
+++ FE/src/components/register/header.jsx
@@ -0,0 +1,11 @@
+import * as RegisterStyled from "../../styles/register";
+
+const Header = () => {
+  return (
+    <RegisterStyled.Header>
+      <RegisterStyled.HeaderTitle> 지문 등록 </RegisterStyled.HeaderTitle>
+    </RegisterStyled.Header>
+  );
+};
+
+export default Header;
--- FE/src/components/register/index.jsx
+++ FE/src/components/register/index.jsx
@@ -1,8 +1,69 @@
+import { useState } from "react";
+import $ from "jquery";
+import * as RegisterStyled from "../../styles/register";
+import Header from "./header";
+
 const Register = () => {
+  const [ grade, setGrade ] = useState();
+  const [ nummber, setNumber ] = useState();
+  const [ order, setOrder ] = useState();
+  const [ name, setName ] = useState("");
+  const [ fingerPrintId, setFingerPrintId ] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    $.ajax({
+      type: "POST",
+      url: "/",
+      data: {
+        id: grade * 1000 + number * 100 + order,
+        grade: grade,
+        class: number,
+        number: order,
+        name: name,
+        fingerprint_id: fingerPrintId,
+      },
+    }).done(() => {
+      console.log("성공");
+    }).fail((result) => {
+      console.log("실패", result);
+    });
+  };
+
+  const Form = () => {
+    return (
+      <>
+        <input
+          value={grade}
+          onChange={(e) => setGrade(e.target.value)}
+          placeholder="학년"
+        />
+        <input
+          value={nummber}
+          onChange={(e) => setNumber(e.target.value)}
+          placeholder="반"
+        />
+        <input
+          value={order}
+          onChange={(e) => setOrder(e.target.value)}
+          placeholder="번호"
+        />
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="이름"
+        />
+        <button onClick={submit}> 등록 </button>
+      </>
+    );
+  };
+
   return (
-    <>
-      브레휴 ㅆ발
-    </>
+    <RegisterStyled.Container>
+      <Header />
+      <Form />
+    </RegisterStyled.Container>
   );
 };
 
