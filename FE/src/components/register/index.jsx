0ca5f5aac18dca1e8c111168908593d25bc972fa
shusbox <shusbox@naver.com>
2025-12-08 15:55:23 +0900

    feat :: add Register Page

--- FE/src/components/register/index.jsx
+++ FE/src/components/register/index.jsx
@@ -1,12 +1,79 @@
+import { useState } from "react";
 import * as RegisterStyled from "../../styles/register"
 import { FingerPrintIcon } from "../../assets/icons";
 
 const Register = () => {
+  const [ grade, setGrade ] = useState("");
+  const [ classNo, setClassNo ] = useState("");
+  const [ number, setNumber ] = useState("");
+  const [ name, setName ] = useState("");
+  const [ fingerPrintId, setFingerPrintId ] = useState("");
+
   return (
     <>
       <RegisterStyled.Backdrop />
       <RegisterStyled.Container>
-        
+        <RegisterStyled.Header>
+          <RegisterStyled.TitleWrapper>
+            <RegisterStyled.IconContainer> <FingerPrintIcon size={"16"} /> </RegisterStyled.IconContainer>
+            <RegisterStyled.Title> 지문 등록 </RegisterStyled.Title>
+          </RegisterStyled.TitleWrapper>
+          <RegisterStyled.Line />
+        </RegisterStyled.Header>
+        <RegisterStyled.Form>
+          <RegisterStyled.Field>
+            <RegisterStyled.Content>
+              <RegisterStyled.ContentHeader> 학생 정보 </RegisterStyled.ContentHeader>
+              <RegisterStyled.ContentBody> 학생 정보를 입력하세요. </RegisterStyled.ContentBody>
+            </RegisterStyled.Content>
+            <RegisterStyled.InputContainer>
+              <RegisterStyled.InputContent>
+                <RegisterStyled.InputWrapper>
+                  <RegisterStyled.Label htmlFor="grade"> 학년 </RegisterStyled.Label>
+                  <RegisterStyled.Input
+                    id="grade"
+                    value={grade}
+                    onChange={(event) => setGrade(event.target.value)}
+                    placeholder="학년"
+                  />
+                </RegisterStyled.InputWrapper>
+                <RegisterStyled.InputWrapper>
+                  <RegisterStyled.Label htmlFor="classNo"> 반 </RegisterStyled.Label>
+                  <RegisterStyled.Input
+                    id="classNo"
+                    placeholder="반"
+                  />
+                </RegisterStyled.InputWrapper>
+                <RegisterStyled.InputWrapper>
+                  <RegisterStyled.Label htmlFor="number"> 번호 </RegisterStyled.Label>
+                  <RegisterStyled.Input
+                    id="number"
+                    placeholder="번호"
+                  />
+                </RegisterStyled.InputWrapper>
+              </RegisterStyled.InputContent>
+              <RegisterStyled.InputWrapper>
+                <RegisterStyled.Label htmlFor="name"> 이름 </RegisterStyled.Label>
+                <RegisterStyled.Input
+                  id="name"
+                  placeholder="이름을 입력하세요"
+                />
+              </RegisterStyled.InputWrapper>
+            </RegisterStyled.InputContainer>
+          </RegisterStyled.Field>
+          <RegisterStyled.Line />
+          <RegisterStyled.Field>
+            <RegisterStyled.Content>
+              <RegisterStyled.ContentHeader> 지문 인식 </RegisterStyled.ContentHeader>
+              <RegisterStyled.ContentBody> 지문 칸을 눌러 지문을 인식하세요. </RegisterStyled.ContentBody>
+            </RegisterStyled.Content>
+            <RegisterStyled.Scan />
+          </RegisterStyled.Field>
+          <RegisterStyled.ButtonContainer>
+            <RegisterStyled.CancelButton> 취소 </RegisterStyled.CancelButton>
+            <RegisterStyled.offActiveButton> 지문 등록하기 </RegisterStyled.offActiveButton>
+          </RegisterStyled.ButtonContainer>
+        </RegisterStyled.Form>
       </RegisterStyled.Container>
     </>
   );
--- FE/src/styles/register.js
+++ FE/src/styles/register.js
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled';
 
+export const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: var(--natural-300);
+`;
+
 export const Backdrop = styled.div`
   width: 100vw;
   height: 100vh;
@@ -11,6 +17,7 @@ export const Backdrop = styled.div`
 `;
 
 export const Container = styled.div`
+  width: 720px;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -21,5 +28,144 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleWrapper = styled.div`
+  padding: 16px 20px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: var(--natural-900);
+`;
+
+export const IconContainer = styled.div`
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--natural-700);
+  border-radius: 9999px;
+  background-color: var(--natural-200);
+`;
+
+export const Title = styled.h1`
+  font-size: 22px;
+  font-weight: 500;
+`;
+
+export const Form = styled.form`
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const Field = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const ContentHeader = styled.h3`
+  margin: 0;
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const ContentBody = styled.p`
+  margin: 0;
+  color: var(--natural-700);
+  font-weight: 300;
+`;
+
+export const InputContainer = styled.div`
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const InputContent = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const Label = styled.label`
+  font-size: 14px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 12px;
+  color: var(--natural-900);
+  font-size: 14px;
+  font-family: 'Pretendard';
+  border: 1px solid var(--natural-300);
+  border-radius: 4px;
+  background-color: var(--white);
+  box-sizing: border-box;
+
+  &::placeholder {
+    color: var(--natural-700);
+    font-size: 14px;
+    font-family: 'Pretendard';
+  }
+`;
+
+export const Scan = styled.div`
+  width: 120px;
+  height: 120px;
+  border: 2px solid var(--natural-300);
+  border-radius: 16px;
+  background-color: var(--natural-100);
+`;
+
+export const ButtonContainer = styled.div`
+  align-self: flex-end;
+  display: flex;
+  gap: 8px;
+`;
+
+export const offActiveButton = styled.button`
+  padding: 12px;
+  color: var(--white);
+  font-size: 15px;
+  font-family: 'Pretendard';
+  border: none;
+  border-radius: 4px;
+  background-color: var(--natural-400);
+`;
+
+export const onActiveButton = styled.button`
+  padding: 12px;
+  color: var(--white);
+  font-size: 15px;
+  font-family: 'Pretendard';
+  border: none;
+  border-radius: 4px;
+  background-color: var(--primary-500);
+`;
+
+export const CancelButton = styled.button`
+  padding: 12px;
+  color: var(--natural-900);
+  font-size: 15px;
+  font-family: 'Pretendard';
+  border: 1px solid var(--natural-300);
+  border-radius: 4px;
+  background-color: var(--natural-100);
 `;
