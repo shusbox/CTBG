57676c100fe3d4a0311a41f25889abda33c35b37
allium0917 <sistus0917@gmail.com>
2025-12-08 16:04:43 +0900

    fix :: 변수 수정

--- BE/routes/students.js
+++ BE/routes/students.js
@@ -2,10 +2,27 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../src/db');
 
-// 학생 목록 조회
 router.get('/', async (req, res) => {
   try {
-    const result = await pool.query('select * from students');
+    const query = `
+      SELECT 
+          a.student_id AS id,
+          s.grade,
+          s.class_number AS number,
+          s.name,
+          a.entry_time::date AS date,
+          a.entry_time::time AS time,
+          CASE 
+              WHEN a.entry_time IS NOT NULL AND a.exit_time IS NULL THEN 'entry'
+              ELSE 'exit'
+          END AS records,
+          a.device_id AS device
+      FROM access_records a
+      JOIN students s ON a.student_id = s.student_id
+      ORDER BY a.entry_time DESC;
+    `;
+
+    const result = await pool.query(query);
     res.json(result.rows);
   } catch (err) {
     console.error(err);
--- BE/src/db.js
+++ BE/src/db.js
@@ -1,43 +1,16 @@
-// require('dotenv').config();
-// import 'dovtenv/config';
-// const { Pool } = require('pg');
-
-// // 필요 패키지 설치
-// // npm install pg onoff
-
-// const { Gpio } = require('onoff'); // GPIO 제어
-// const { Client } = require('pg');  // PostgreSQL
-
-// // ==============================
-// // 지문 센서 테스트용 GPIO
-// const FINGER_PIN = 18;
-// const fingerSensor = new Gpio(FINGER_PIN, 'in', 'both');
-// // ==============================
-
-// // ==============================
-// // PostgreSQL 연결 설정
-// const DB_CONFIG = {
-//   host: 'localhost',
-//   port: 5432,
-//   database: 'root',
-//   user: 'root',
-//   password: '1q2w3e',
-// };
-
-// // ==============================
-
-// // 연결 테스트
-// pool.connect()
-//   .then(client => {
-//     console.log('PostgreSQL (Docker) 연결 성공');
-//     client.release();
-//   })
-//   .catch(err => console.error('DB 연결 실패', err));
-
-// module.exports = pool;
-
 require('dotenv').config();
+import 'dovtenv/config';
 const { Pool } = require('pg');
+
+// 필요 패키지 설치
+// npm install pg onoff
+
+const { Gpio } = require('onoff'); // GPIO 제어
+const { Client } = require('pg');  // PostgreSQL
+
+// 지문 센서 테스트용 GPIO
+const FINGER_PIN = 18;
+const fingerSensor = new Gpio(FINGER_PIN, 'in', 'both');
 
 // PostgreSQL 연결 설정
 const DB_CONFIG = {
