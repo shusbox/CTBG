41b87f81bfcaa630047d8fcdf5ebdc23ee710a27
shusbox <shusbox@naver.com>
2025-12-08 17:49:15 +0900

    fix :: Link 수정

--- FE/src/components/sidebar/nav.jsx
+++ FE/src/components/sidebar/nav.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { DoorExitIcon, FingerPrintIcon } from "../../assets/icons";
 import * as SidebarStyled from "../../styles/sidebar";
 
@@ -13,8 +14,8 @@ const Item = ({ icon, content }) => {
 const Nav = () => {
   return (
     <SidebarStyled.Nav>
-      <Item icon={ <DoorExitIcon size={"20"} color="currentColor" /> } content={"출입 기록"} />
-      <Item icon={ <FingerPrintIcon size={"20"} color="currentColor" /> } content={"지문 등록"} />
+      <Link path="/entry"> <Item icon={ <DoorExitIcon size={"20"} color="currentColor" /> } content={"출입 기록"} /> </Link>
+      <Link path="/register"> <Item icon={ <FingerPrintIcon size={"20"} color="currentColor" /> } content={"지문 등록"} /> </Link>
     </SidebarStyled.Nav>
   );
 };
